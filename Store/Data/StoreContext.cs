5b7aa21679096ec9fc4d55ef3acbbca7c4c92fc3
Seva Zakharchenko <vseva80@yandex.ru>
2019-01-22 12:30:31 +0200

    Add project files.

---
+++ Store/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Store.Models
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext (DbContextOptions<StoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Store.Models.Product> Product { get; set; }
+    }
+}
